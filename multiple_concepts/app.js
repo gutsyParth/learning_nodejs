331b9eb4c3aa817c18e316f3c9f4dc16f3d3620c
Parth Yadav <gutsyparthyadav@gmail.com>
2026-03-06 17:40:40 +0530

    learn multiple concepts

---
+++ multiple_concepts/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const morgan = require("morgan");
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
+//MIDDLEWARES
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Hello from the Middleware :)");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
